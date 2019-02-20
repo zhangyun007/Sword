bb9459a4325e463867f5de62b74e110dd8964f6f
zhangyun007 <47809648+zhangyun007@users.noreply.github.com>
2019-02-20 15:50:06 +0800

    Add files via upload

---
+++ Php/test.php
@@ -0,0 +1,19 @@
+<?php
+
+define("GREETING","Hello world!\n");
+echo constant("GREETING");
+	
+$array = array(
+    "foo" => "bar",
+    42    => 24,
+    "multi" => array(
+         "dimensional" => array(
+             "array" => "foo"
+         )
+    )
+);
+
+var_dump($array["foo"]);
+var_dump($array[42]);
+var_dump($array["multi"]["dimensional"]["array"]);
+?>
