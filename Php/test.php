172c13884b66f4da2632d01e18a1aa137a5dfa7b
zhangyun007 <test3ds@qq.com>
2019-02-21 17:51:06 +0800

    mod

--- Php/test.php
+++ Php/test.php
@@ -1,10 +1,21 @@
 <?php
 
-/* PHP数组，key-value数组*/
-
 define("GREETING","Hello world!\n");
 echo constant("GREETING");
-	
+
+/* PHP数组，key-value数组*/	
+
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
+print_r($array);
+
+echo "-------------------\n";
+
 $array = array(
     "foo" => "bar",
     42    => 24,
@@ -19,10 +30,14 @@ var_dump($array["foo"]);
 var_dump($array[42]);
 var_dump($array["multi"]["dimensional"]["array"]);
 
+echo "-------------------\n";
+
 $arr = array( "A", "B", "C" );
 unset( $arr[1] );
 var_dump($arr);
 print_r($arr);
+
+echo "-------------------\n";
 
 $age=array("Bill"=>"63","Steve"=>"56","Elon"=>"47");
 
@@ -31,6 +46,9 @@ foreach($age as $key=>$value) {
    echo "\n";
 }
 
+echo "-------------------\n";
+
+/*二维数组*/
 $cars = array
   (
   array("Volvo",22,18, 13),
@@ -39,5 +57,30 @@ $cars = array
   array("Land Rover",17,15,4)
   );
 
-var_dump($cars)
+var_dump($cars);
+
+echo "-------------------\n";
+
+function getArray() {
+    return array(1, 2, 3);
+}
+
+// on PHP 5.4
+$secondElement = getArray()[1];
+echo $secondElement; 
+echo "\n";
+
+// previously
+$tmp = getArray();
+$secondElement = $tmp[1];
+echo $secondElement;
+echo "\n";
+
+// or
+list(, $secondElement) = getArray();
+echo $secondElement;
+echo "\n";
+
+echo "-------------------\n";
+
 ?>
