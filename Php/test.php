c505d4f8e60784279b26d27ad211c582f8ff836b
zhangyun007 <test3ds@qq.com>
2019-02-22 13:53:20 +0800

    imod

--- Php/test.php
+++ Php/test.php
@@ -32,10 +32,24 @@ var_dump($array["multi"]["dimensional"]["array"]);
 
 echo "-------------------\n";
 
-$arr = array( "A", "B", "C" );
-unset( $arr[1] );
-var_dump($arr);
-print_r($arr);
+// 创建一个简单的数组
+$array = array(1, 2, 3, 4, 5);
+print_r($array);
+
+// 现在删除其中的所有元素，但保持数组本身不变:
+foreach ($array as $i => $value) {
+    unset($array[$i]);
+}
+print_r($array);
+
+// 添加一个单元（注意新的键名是 5，而不是你可能以为的 0）
+$array[] = 6;
+print_r($array);
+
+// 重新索引：
+$array = array_values($array);
+$array[] = 7;
+print_r($array);
 
 echo "-------------------\n";
 
