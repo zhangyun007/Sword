8ec6ccfbd186db69083c59c122b775872ff6823c
zhangyun007 <zhangxp147@qq.com>
2019-03-11 11:29:08 +0800

    mod

--- SQL/sqlite3test.sql
+++
@@ -1,8 +0,0 @@
-.open test.db
-.databases
-.table
-.schema
-drop table test;
-CREATE TABLE test (name char(10));
-insert into test (name) values('zhangfei'), ('liubei'), ('zhugeliang'), ('caocao'), ('guanyu');
-select * from test;
