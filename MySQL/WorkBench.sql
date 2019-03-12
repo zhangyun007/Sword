437dd0c5a41750336ae835575a922d3ffc0ca20b
zhangyun007 <zhangxp147@qq.com>
2019-03-12 11:47:49 +0800

    mod

--- MySQL/WorkBench.sql
+++ MySQL/WorkBench.sql
@@ -1,8 +1,13 @@
--- mysql用户管理程序
 show databases;
--- 创建用户zhangsan，密码为zhangsan
+
+/*
+创建用户 CREATE USER  'user_name'@'host'  IDENTIFIED BY  'password'；
+user_name：要创建用户的名字。
+host：表示要这个新创建的用户允许从哪台机登陆，如果只允许从本机登陆，则填‘localhost’，如果允许从远程登陆，则填 ‘%’。
+password：新创建用户的登陆数据库密码，如果没密码可以不写。
+*/
+
 create user zhangsan identified by 'zhangsan';
 select * from mysql.user;
--- 删除用户zhangsan,一般后接@localhost，这里是%
 DROP USER zhangsan@'%';
 select * from mysql.user;
