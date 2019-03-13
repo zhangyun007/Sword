f0818b8580bd10f50d25672d9e55f8fb73a1ea02
zhangyun007 <zhangxp147@qq.com>
2019-03-13 21:45:14 +0800

    mod

---
+++ MySQL/user.sql
@@ -0,0 +1,13 @@
+show databases;
+
+/*
+创建用户 CREATE USER  'user_name'@'host'  IDENTIFIED BY  'password'；
+user_name：要创建用户的名字。
+host：表示要这个新创建的用户允许从哪台机登陆，如果只允许从本机登陆，则填‘localhost’，如果允许从远程登陆，则填 ‘%’。
+password：新创建用户的登陆数据库密码，如果没密码可以不写。
+*/
+
+create user zhangsan identified by 'zhangsan';
+select * from mysql.user;
+DROP USER zhangsan@'%';
+select * from mysql.user;
