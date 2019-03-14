44f19ca712d51f2cb67ae447697b0a51be6bc6ae
zhangyun007 <zhangxp147@qq.com>
2019-03-14 10:54:28 +0800

    mod

--- MySQL/first.sql
+++ MySQL/first.sql
@@ -3,10 +3,15 @@ use testdb;
 
 show engine innodb status;
 
+/*大约是查看字符集相关的信息，具体含义不明。*/
+show character set;
+show collation like 'utf8%';
+show variables like 'character_set_server';
+
+
 CREATE TABLE test (c1 float(10,2),c2 decimal(10,2));
 insert into test values(131072.32, 131072.32);
 select * from test;
-
 
 /*
  *memory表存储在RAM里。当服务器关闭时，存储在MEMORY表里的数据丢失。
@@ -15,12 +20,6 @@ select * from test;
  */
 CREATE TABLE newtest ENGINE = MEMORY select * from test;
 ALTER TABLE newtest ENGINE = InnoDB;
-
-
-/*大约是查看字符集相关的信息，具体含义不明。*/
-show character set;
-show collation like 'utf8%';
-show variables like 'character_set_server';
 
 
 drop database testdb;
