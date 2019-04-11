b937a7580178066ddf7a9f6916e0982b497ba02f
zhangyun007 <zhangxp147@qq.com>
2019-04-11 13:17:42 +0800

    mod

--- Node/parse.js
+++ Node/parse.js
@@ -1,2 +1,21 @@
+/*
+
+minimist模块用于解析命令行参数。
+
+$ node example/parse.js -a beep -b boop
+{ _: [], a: 'beep', b: 'boop' }
+
+$ node example/parse.js -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
+{ _: [ 'foo', 'bar', 'baz' ],
+  x: 3,
+  y: 4,
+  n: 5,
+  a: true,
+  b: true,
+  c: true,
+  beep: 'boop' }
+  
+*/
+
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv);
