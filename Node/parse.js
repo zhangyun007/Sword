5370b769885255275d5d15a9f75795ebb42e4723
zhangyun007 <zhangxp147@qq.com>
2019-04-11 13:19:28 +0800

    mod

--- Node/parse.js
+++ Node/parse.js
@@ -15,6 +15,10 @@ $ node example/parse.js -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
   c: true,
   beep: 'boop' }
   
+# npm install minimist -g
+node parse.js -a beep -b boop
+node parse.js -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
+
 */
 
 var argv = require('minimist')(process.argv.slice(2));
--- Node/test.sh
+++
@@ -1,4 +0,0 @@
-# npm install minimist
-node parse.js -a beep -b boop
-node parse.js -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
-
