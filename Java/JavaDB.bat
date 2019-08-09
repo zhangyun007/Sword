ec8fd66f596bb88499999abd16c39ca3a526073b
zhangyun007 <zhangxp147@qq.com>
2019-08-09 23:31:06 +0800

    mod

--- Java/JavaDB.bat
+++ Java/JavaDB.bat
@@ -1,3 +1,3 @@
-SET DERBY_INSTALL=E:\zhangxp\db-derby-10.15.1.3-bin;
-SET PATH=%DERBY_INSTALL%/BIN;
-SET CLASSPATH=.;%DERBY_INSTALL%/lib/derby.jar;%DERBY_INSTALL%/lib/derbytools.jar
+SET DERBY_INSTALL=E:\zhangxp\db-derby-10.15.1.3-bin
+SET PATH=%PATH%;%DERBY_INSTALL%\bin;
+SET CLASSPATH=.;%DERBY_INSTALL%\lib\derby.jar;%DERBY_INSTALL%\lib\derbytools.jar
