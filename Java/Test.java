0225d7b0a2fc6b308b2b61af8b6ba550c5f30871
zhangyun007 <zhangxp147@qq.com>
2019-08-09 21:10:05 +0800

    mod

---
+++ Java/Test.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class Test {
+	public static void main(String[] args) {
+		Collection<String> col=new ArrayList<String>();
+		col.add("abc");
+		col.add("bcd");
+		
+		Iterator<String> it=col.iterator();
+		
+		while(it.hasNext()){
+				System.out.println(it.next());
+		}    
+		
+		String str = "Hello";
+		System.out.println(str);
+	}
+}
