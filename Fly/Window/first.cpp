3bf10aeca42a9d32a6242097b0fa5524b1aa33d9
zhangyun007 <zhangxp147@qq.com>
2019-09-07 07:39:34 +0800

    mod

---
+++ Fly/Window/first.cpp
@@ -0,0 +1,78 @@
+﻿#include <windows.h>
+#include <prsht.h>
+
+#pragma comment(lib,"User32.lib")
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int ICmdShow) {
+    static TCHAR szAppName[] = TEXT("windowOfC");
+
+    HWND hwnd;//窗体句柄变量
+    MSG msg;//消息变量
+    WNDCLASS wndclass;//窗口类变量
+
+    //配置窗口类结构
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = szAppName;
+
+    //注册窗口类
+    if (!RegisterClass(&wndclass)) {
+        MessageBox(NULL, TEXT("注册窗口类失败！"), szAppName, MB_ICONERROR);
+        return 0;
+    }
+
+    hwnd = CreateWindow(szAppName,
+        TEXT("C语言调用Windows API创建的窗口"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hwnd, ICmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+//消息处理方法，windows操作系统的回调函数
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message) {
+    case WM_CREATE:
+        MessageBox(NULL, TEXT("创建窗口了！"), NULL, MB_ICONERROR);
+        return 0;
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        GetClientRect(hwnd, &rect);
+        DrawText(hdc, TEXT("hello C windows "), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hwnd, &ps);
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
