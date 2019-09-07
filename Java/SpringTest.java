16a9965349dce1edd2ba1b6c3c9af15e297d6e34
zhangyun007 <zhangxp147@qq.com>
2019-09-07 11:54:59 +0800

    mod

---
+++ Java/SpringTest.java
@@ -0,0 +1,21 @@
+package org.crazyit.app.service.personService;
+  
+class PersonService {
+		private String name;
+		public void setName(String name) {
+			this.name = name;
+		}
+		public void info() {
+			System.out.println("His name is: " + name);
+		}
+}
+
+public class SpringTest {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXMLApplicationContext("bean.xml");
+		System.out.println(ctx);
+		
+		PersonService p = ctx.getBean("personService", PersonService.class);
+		p.info();
+	}
+}
