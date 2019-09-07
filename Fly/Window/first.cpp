bc159a42c1bebaddd951fc1b377b56db6835b3ed
zhangyun007 <zhangxp147@qq.com>
2019-09-07 09:44:50 +0800

    mod

--- Fly/Window/first.cpp
+++ Fly/Window/first.cpp
@@ -1,78 +1,120 @@
 ﻿#include <windows.h>
-#include <prsht.h>
+
+//使用CString
+#include <atlstr.h>
 
 #pragma comment(lib,"User32.lib")
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//按钮ID
+#define IDB_ONE     3301  
+#define IDB_TWO     3302  
+#define IDB_THREE   3303  
+ 
+//函数声明
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);  
+  
+LPCSTR WINDOWS_CLASS = "MyWindowClass";    //类名  
+LPCSTR WINDOWS_TITLE = "测试按钮";   //窗口标题  
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int ICmdShow) {
-    static TCHAR szAppName[] = TEXT("windowOfC");
-
-    HWND hwnd;//窗体句柄变量
-    MSG msg;//消息变量
-    WNDCLASS wndclass;//窗口类变量
-
-    //配置窗口类结构
-    wndclass.style = CS_HREDRAW | CS_VREDRAW;
-    wndclass.lpfnWndProc = WndProc;
-    wndclass.cbClsExtra = 0;
-    wndclass.cbWndExtra = 0;
-    wndclass.hInstance = hInstance;
-    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hbrBackground = (HBRUSH)COLOR_WINDOW;
-    wndclass.lpszMenuName = NULL;
-    wndclass.lpszClassName = szAppName;
-
-    //注册窗口类
-    if (!RegisterClass(&wndclass)) {
-        MessageBox(NULL, TEXT("注册窗口类失败！"), szAppName, MB_ICONERROR);
-        return 0;
+CString str("abcd");
+ 
+int WINAPI wWinMain(HINSTANCE hThisApp,  
+    HINSTANCE hPrevApp,  
+    LPWSTR lpCmd,  
+    int nShow)  
+{  
+    WNDCLASSEX wc = { };  
+    wc.cbSize = sizeof(WNDCLASSEX);  
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;  
+    wc.hInstance = hThisApp;  
+    wc.lpfnWndProc = (WNDPROC)WindowProc;  
+    wc.lpszClassName = WINDOWS_CLASS;  
+    wc.style = CS_HREDRAW | CS_VREDRAW;  
+    RegisterClassEx(&wc);  
+    
+    
+    HWND hwnd = CreateWindowEx(WS_EX_WINDOWEDGE,  
+                    WINDOWS_CLASS,  
+                    WINDOWS_TITLE,  
+                    WS_OVERLAPPEDWINDOW,  
+                    20,  
+                    25,  
+                    400,  
+                    300,  
+                    NULL,  
+                    NULL,  
+                    hThisApp,  
+                    NULL);  
+    if (hwnd == NULL)  
+    {
+        return -1;
     }
-
-    hwnd = CreateWindow(szAppName,
-        TEXT("C语言调用Windows API创建的窗口"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        NULL,
-        NULL,
-        hInstance,
-        NULL);
-
-    ShowWindow(hwnd, ICmdShow);
-    UpdateWindow(hwnd);
-
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return msg.wParam;
-}
-
-//消息处理方法，windows操作系统的回调函数
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    HDC hdc;
-    PAINTSTRUCT ps;
-    RECT rect;
-
-    switch (message) {
-    case WM_CREATE:
-        MessageBox(NULL, TEXT("创建窗口了！"), NULL, MB_ICONERROR);
-        return 0;
-    case WM_PAINT:
-        hdc = BeginPaint(hwnd, &ps);
-        GetClientRect(hwnd, &rect);
-        DrawText(hdc, TEXT("hello C windows "), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-        EndPaint(hwnd, &ps);
-        return 0;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-
-    return DefWindowProc(hwnd, message, wParam, lParam);
-}
+          
+    ShowWindow(hwnd, nShow);  
+    UpdateWindow(hwnd);  
+ 
+    MSG msg;  
+    while(GetMessage(&msg,NULL,0,0))  
+    {  
+        TranslateMessage(&msg);  
+        DispatchMessage(&msg);  
+    }  
+    
+    return 0;  
+}  
+  
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)  
+{  
+    switch(msg) {  
+	
+    case WM_DESTROY:  
+        PostQuitMessage(0);  
+        return 0;  
+ 
+    case WM_CREATE: 
+		{  
+			MessageBox(hwnd, "ФЫВФЫ", "提示", MB_OK | MB_ICONINFORMATION);  
+			
+            //创建三个按钮  
+            CreateWindow("MDICLIENT", "按钮一", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  
+                35, 10, 160, 60, hwnd, (HMENU)IDB_ONE, NULL, NULL);  
+                
+            CreateWindow("LISTBOX", "按钮二", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  
+                35, 80, 160, 60, hwnd, (HMENU)IDB_TWO, NULL, NULL); 
+                 
+            CreateWindow("Button", "按钮三", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  
+                35, 150, 160, 60, hwnd, (HMENU)IDB_THREE, NULL, NULL);  
+		}  
+		return 0; 
+         
+    case WM_COMMAND:  
+        {  
+            switch(LOWORD(wParam))  
+            {  
+            case IDB_ONE:  
+                //MessageBox(hwnd, L"您点击了第一个按钮。", L"提示", MB_OK | MB_ICONINFORMATION);  
+                SendMessage((HWND)lParam, WM_SETTEXT, (WPARAM)NULL, (LPARAM)"First");  
+                break;  
+                
+            case IDB_TWO:  
+                //MessageBox(hwnd, L"您点击了第二个按钮。", L"提示", MB_OK | MB_ICONINFORMATION);  
+                SendMessage((HWND)lParam, WM_SETTEXT, (WPARAM)NULL, (LPARAM)"Second");  
+                break;  
+                
+            case IDB_THREE:  
+                //MessageBox(hwnd, L"您点击了第三个按钮。", L"提示", MB_OK | MB_ICONINFORMATION);  
+                SendMessage((HWND)lParam, WM_SETTEXT, (WPARAM)NULL, (LPARAM)"第三个按鈕已点击");  
+                break;  
+                
+            default:  
+                break;  
+            }  
+        }  
+        return 0;  
+        
+    default:  
+        return DefWindowProc(hwnd,msg,wParam,lParam);  
+    }  
+    
+    return 0;  
+}  
