2d7479aeb26ad8675e9e6898200c57e357ce2e20
zhangyun007 <zhangxp147@qq.com>
2019-09-10 06:18:45 +0800

    mod

--- Delphi/Calculator/unit1.pas
+++ Delphi/Calculator/unit1.pas
@@ -13,26 +13,26 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
     ComboBox1: TComboBox;
     Edit1: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
     Memo1: TMemo;
-    Panel1: TPanel;
-    Panel2: TPanel;
     SpeedButton1: TSpeedButton;
-    procedure BitBtn1Click(Sender: TObject);
-    procedure BitBtn2Click(Sender: TObject);
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton6: TSpeedButton;
     procedure Button1Click(Sender: TObject);
     procedure Button1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
-    procedure Edit4Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Panel1Click(Sender: TObject);
-    procedure Panel2Click(Sender: TObject);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
     procedure SpeedButton1Click(Sender: TObject);
-    procedure ToggleBox1Change(Sender: TObject);
+    procedure SpeedButton3Click(Sender: TObject);
+    procedure SpeedButton4Click(Sender: TObject);
+    procedure SpeedButton6Click(Sender: TObject);
   private
 
   public
@@ -59,15 +59,6 @@ begin
   Memo1.Text := Memo1.Text + Edit1.Text + ComboBox1.Text + Edit2.Text + '=' + Edit3.Text + chr(13) + chr(10);     // \r\n
 end;
 
-procedure TForm1.BitBtn1Click(Sender: TObject);
-begin
-  Edit1.Text := Edit3.Text;
-end;
-
-procedure TForm1.BitBtn2Click(Sender: TObject);
-begin
-  Edit2.Text := Edit3.Text;
-end;
 
 procedure TForm1.Button1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
@@ -78,25 +69,20 @@ begin
   end;
 end;
 
-procedure TForm1.Edit4Click(Sender: TObject);
-begin
- Edit1.Text := Edit3.Text;
-end;
-
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   Memo1.Text := '';
 end;
 
-procedure TForm1.Panel1Click(Sender: TObject);
+procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
 begin
-  Edit1.Text := Edit3.Text;
-end;
-
-procedure TForm1.Panel2Click(Sender: TObject);
-begin
-  Edit2.Text := Edit3.Text;
+  if Button=Mbleft then
+  begin
+    ReleaseCapture;
+    PostMessage(Self.Handle,WM_SYSCOMMAND,SC_MOVE+1,0);
+  end;
 end;
 
 procedure TForm1.SpeedButton1Click(Sender: TObject);
@@ -104,9 +90,25 @@ begin
   Form1.Close;
 end;
 
-procedure TForm1.ToggleBox1Change(Sender: TObject);
+procedure TForm1.SpeedButton3Click(Sender: TObject);
 begin
+ case ComboBox1.Text of
+ '+': Edit3.Text:= FloatToStr(StrToFloat(Edit1.Text) + StrToFloat(Edit2.Text));
+ '-': Edit3.Text:= FloatToStr(StrToFloat(Edit1.Text) - StrToFloat(Edit2.Text));
+ '*': Edit3.Text:= FloatToStr(StrToFloat(Edit1.Text) * StrToFloat(Edit2.Text));
+ '/': Edit3.Text:= FloatToStr(StrToFloat(Edit1.Text) / StrToFloat(Edit2.Text)); //div
+ end;
+ Memo1.Text := Memo1.Text + Edit1.Text + ComboBox1.Text + Edit2.Text + '=' + Edit3.Text + chr(13) + chr(10);     // \r\n
+end;
 
+procedure TForm1.SpeedButton4Click(Sender: TObject);
+begin
+   Edit1.Text := Edit3.Text;
+end;
+
+procedure TForm1.SpeedButton6Click(Sender: TObject);
+begin
+  Edit2.Text := Edit3.Text;
 end;
 
 end.
