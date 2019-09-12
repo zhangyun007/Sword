9b85af73e561e63441f67741d13632c70319a439
zhangyun007 <zhangxp147@qq.com>
2019-09-12 12:38:46 +0800

    mod

--- Python/ext/Visualization/testnum.py
+++ Python/ext/Visualization/testnum.py
@@ -1,13 +1,17 @@
 from time import clock
+
+# 给numpy其一个别名叫np
 import numpy as np
 from math import sin
 
+# 列表中保存的是对象指针
 t = [i * 0.0001 for i in range(10000000)]
 start_time = clock()
 for i, item in enumerate(t):
     t[i] = sin(item)
 print('用时为:', clock() - start_time)
 
+# np.array保存对象本身，所以快。
 t = [i * 0.0001 for i in range(10000000)]
 start_time = clock()
 t = np.array(t)
