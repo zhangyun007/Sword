3878f09ff2fb2068c000c9447566bc2af2b77613
zhangyun007 <zhangxp147@qq.com>
2019-09-13 13:11:46 +0800

    mod

--- Fly/STL/list2.cpp
+++ Fly/STL/list2.cpp
@@ -5,18 +5,22 @@ using namespace std;
 
 int main()
 {
-	list<char> coll;      // list container for character elements
+	// list container for character elements
+	list<char> coll;      
 
 	// append elements from 'a' to 'z'
 	for (char c='a'; c<='z'; ++c) {
 		coll.push_back(c);
 	}
 
-	/* print all elements
-	 * - iterate over all elements
-	 */
 	list<char>::const_iterator pos;
 	for (pos = coll.begin(); pos != coll.end(); ++pos) {
+		cout << *pos << ' ';
+	}
+	cout << "\n";
+	
+	//用正向迭代器倒序输出。
+	for (pos = --coll.end(); pos != --coll.begin(); --pos) {
 		cout << *pos << ' ';
 	}
 	cout << endl;
