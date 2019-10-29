3d37e4f9d8e57c4045f9645cb70e825e0ab73c6e
zhangyun007 <zhangxp147@qq.com>
2019-10-29 18:57:35 +0800

    mod

--- Fly/Window/rain.cpp
+++ Fly/Window/rain.cpp
@@ -1,8 +1,50 @@
-﻿#include <windows.h>
-#include <d3d9.h>
-#include <d3dx9.h>
+﻿//WinXP用DirectX9.0,Win7不支持9.0
+//Dirext11 SDK下载地址：http://download.microsoft.com/download/F/1/7/F178BCE4-FA19-428F-BB60-F3DEE1130BFA/DXSDK_Feb10.exe
+
+#pragma once
+
+#include <Windows.h>
+
+#pragma region Undefine Windows Macros
+// Only undefine, if DXGIType.h has not been included yet
+    #ifndef __dxgitype_h__
+        #undef DXGI_STATUS_OCCLUDED
+        #undef DXGI_STATUS_CLIPPED
+        #undef DXGI_STATUS_NO_REDIRECTION
+        #undef DXGI_STATUS_NO_DESKTOP_ACCESS
+        #undef DXGI_STATUS_GRAPHICS_VIDPN_SOURCE_IN_USE
+        #undef DXGI_STATUS_MODE_CHANGED
+        #undef DXGI_STATUS_MODE_CHANGE_IN_PROGRESS
+        #undef DXGI_ERROR_INVALID_CALL
+        #undef DXGI_ERROR_NOT_FOUND
+        #undef DXGI_ERROR_MORE_DATA
+        #undef DXGI_ERROR_UNSUPPORTED
+        #undef DXGI_ERROR_DEVICE_REMOVED
+        #undef DXGI_ERROR_DEVICE_HUNG
+        #undef DXGI_ERROR_DEVICE_RESET
+        #undef DXGI_ERROR_WAS_STILL_DRAWING
+        #undef DXGI_ERROR_FRAME_STATISTICS_DISJOINT
+        #undef DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE
+        #undef DXGI_ERROR_DRIVER_INTERNAL_ERROR
+        #undef DXGI_ERROR_NONEXCLUSIVE
+        #undef DXGI_ERROR_NOT_CURRENTLY_AVAILABLE
+        #undef DXGI_ERROR_REMOTE_CLIENT_DISCONNECTED
+        #undef DXGI_ERROR_REMOTE_OUTOFMEMORY
+        #undef D3D11_ERROR_TOO_MANY_UNIQUE_STATE_OBJECTS
+        #undef D3D11_ERROR_FILE_NOT_FOUND
+        #undef D3D11_ERROR_TOO_MANY_UNIQUE_VIEW_OBJECTS
+        #undef D3D11_ERROR_DEFERRED_CONTEXT_MAP_WITHOUT_INITIAL_DISCARD
+        #undef D3D10_ERROR_TOO_MANY_UNIQUE_STATE_OBJECTS
+        #undef D3D10_ERROR_FILE_NOT_FOUND
+    #endif
+#pragma endregion
+
+#include <D3D11.h>
+#include <d3dx11.h>
+
 #include <strsafe.h>
 #include <time.h>
+
 #define PARTICLENUM 5000
 struct CUSTOMVERTEX
 { 
--- Python/ext/mouse/first.py
+++ Python/ext/mouse/first.py
@@ -2,7 +2,7 @@
 
 import pyautogui
 
-for i in range(2):
+for i in range(1):
    pyautogui.moveTo(300, 300, duration=0.25)
    pyautogui.moveTo(400, 300, duration=0.25)
    pyautogui.moveTo(400, 400, duration=0.25)
@@ -10,7 +10,21 @@ for i in range(2):
 
 #Windows开始菜单按钮的位置   
 pyautogui.click(x=29, y=747)
-time.sleep(1.0)
-pyautogui.click(x=29, y=747)
-time.sleep(1.0)
-pyautogui.click(clicks=2) 
+
+#Dota2任务栏，只有一个任务的坐标
+pyautogui.click(x=130, y=750)
+
+#Dota2匹配按钮的坐标
+#pyautogui.click(x=860, y=740)
+
+
+#按两下F1，选中英雄
+pyautogui.press('f1')
+
+#Dota2最后一个技能，快捷键为r键。
+#pyautogui.keyDown('R')
+
+#这里使用技能图标的坐标
+#pyautogui.click(x=590, y=690)
+
+pyautogui.click(x=400, y=400)
