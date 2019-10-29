944ab7c8a53e0ba7cff1a0512b023ca0a62bc9b4
zhangyun007 <zhangxp147@qq.com>
2019-10-29 20:56:41 +0800

    mod

--- Python/ext/mouse/first.py
+++ Python/ext/mouse/first.py
@@ -11,15 +11,21 @@ for i in range(1):
 #Windows开始菜单按钮的位置   
 pyautogui.click(x=29, y=747)
 
-#Dota2任务栏，只有一个任务的坐标
+#Windows任务栏第一个任务
 pyautogui.click(x=130, y=750)
+
+#点击Firefox地址栏
+pyautogui.click(x=360, y=50)
+
+#输入访问地址
+pyautogui.typewrite('http://baidu.com', 0.25)  # 0.25表示每输完一个字符串延时0.25秒
 
 #Dota2匹配按钮的坐标
 #pyautogui.click(x=860, y=740)
 
 
 #按两下F1，选中英雄
-pyautogui.press('f1')
+#pyautogui.press('f1')
 
 #Dota2最后一个技能，快捷键为r键。
 #pyautogui.keyDown('R')
@@ -27,4 +33,4 @@ pyautogui.press('f1')
 #这里使用技能图标的坐标
 #pyautogui.click(x=590, y=690)
 
-pyautogui.click(x=400, y=400)
+#pyautogui.click(x=400, y=400)
