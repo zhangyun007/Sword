a89955d1c6b246ecb182e05e891b45cf5d641ecc
zhangyun007 <zhaoyun004@qq.com>
2020-01-23 23:29:11 +0800

    mod

---
+++ Old/clean.bat
@@ -0,0 +1,26 @@
+cd Java
+del *.class
+cd ..
+
+
+cd Fly
+del *.exe *.obj
+
+cd Window
+del *.exe *.obj
+cd ..
+
+cd STL
+del *.exe *.obj
+
+cd TinySTL
+del *.exe *.obj
+cd .. 
+
+cd "Associate Containers"
+del *.exe *.obj
+cd ..
+
+cd ..
+
+cd ..
