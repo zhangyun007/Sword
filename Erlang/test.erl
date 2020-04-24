97f2a8bd1e26d07e48971cf054ddc4fe1ea50b31
zhangyun007 <47809648+zhangyun007@users.noreply.github.com>
2020-04-24 21:38:15 +0800

    ''

--- Erlang/test.erl
+++ Erlang/test.erl
@@ -13,5 +13,12 @@ main([Str1,Str2,Str3]) ->
 main(_) ->
 	Pi = math:pi(),
 	io:format("Pi is ~5.2f\n", [Pi]),
-	io:format("zero args~n").
+	io:format("zero args~n"),
+	io:format("Pi is ~10#\n", [len([1,2,3,4,7])]).
+
+len(L) -> len(L,0).   %% 这其实只是给 len/2 的第二个参数设置了一个默认值 0.
+len([], Acc) -> Acc;  %% 所有的元素都读完了
+len([_|T], Acc) -> len(T,Acc+1).  %% 读一个元素，Acc 增1，然后计算剩下的 List 的长度。\
+
+%% 注意，函数语句结尾“. ; ,”。  
 
