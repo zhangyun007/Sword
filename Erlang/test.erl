f24265d30866ac3132bfebad4b89cf021508f82e
zhangyun007 <47809648+zhangyun007@users.noreply.github.com>
2020-04-24 17:36:55 +0800

    ''

--- Erlang/test.erl
+++ Erlang/test.erl
@@ -1,11 +1,17 @@
 #!/usr/bin/env escript
 %%! -smp enable -sname mmcshadow -mnesia debug verbose[/color]
+
 main([String]) ->
   io:format("args ~p~n",[String]);
+  
 main([Str1,Str2]) ->
   io:format("two args ~p ~p~n",[Str1,Str2]);
+
 main([Str1,Str2,Str3]) ->
   io:format("three args ~p ~p ~p ~p~n",[Str1,Str2,Str3,node()]);
+
 main(_) ->
-  io:format("zero args~n").
+	Pi = math:pi(),
+	io:format("Pi is ~5.2f\n", [Pi]),
+	io:format("zero args~n").
 
