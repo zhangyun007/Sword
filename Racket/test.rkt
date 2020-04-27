7c68f53630234cec412c00b65e7d378a682cd4de
zhangyun007 <zhaoyun004@qq.com>
2020-04-27 22:36:51 +0800

    mod

--- Racket/test.rkt
+++ Racket/test.rkt
@@ -14,3 +14,14 @@
 (define newdisplay (lambda (x) (begin (display x)(newline))))
 (define tt (lambda args (for-each newdisplay args)))
 (tt 'abc 'efg 'tomson)
+
+
+;map和for-each格式相同。map返回一个值，for-each没有返回值。
+
+(map (lambda (x) (* x x)) ‘(1 2 3))
+
+(map + ‘(1 3 5)‘ (2 4 6))
+(map (lambda (x y) (* x y))‘(1 2 3)‘(2 4 6))
+
+(define sum 0)
+(for-each (lambda (x) (set! sum (+ sum x))) ‘(1 2 3 4))
