7dffe54062d1f0cc06bd2410c2bf259cf0b2f978
zhangyun007 <zhaoyun004@qq.com>
2020-05-01 21:13:54 +0800

    mod

--- Python/easy/easy.py
+++ Python/easy/easy.py
@@ -1,3 +1,9 @@
+a = [12, 23, 34, 345, 345, 234 , 234, 435, 345];
+print(a[2:4])
+del(a[2:4]);
+a.remove(234)
+
+
 # 半开半闭区间。包括左边，不包括右边。
 
 bigmuls=[]
@@ -11,13 +17,28 @@ print(bigmuls)
 
 print([(x,y) for x in (1,2,3,4) for y in (10,15,3,22) if x*y > 25]) 
 
-L = [lambda x: x+2, lambda x: x*2, lambda x: x**2]
-print "L=", L[0](1), L[1](2), L[2](3)
 
-a = [12, 23, 34, 345, 345, 234 , 234, 435, 345];
-print(a[2:4])
-del(a[2:4]);
-a.remove(234)
+
+def f(x): return x % 2 != 0 and x % 3 != 0
+a = filter(f, range(2, 100))
+print(list(a))
+
+def cube(x): return x*x*x;
+a = map(cube, range(1,11))
+print(list(a))
+
+
+
+funa = lambda x: x + 1
+print(funa(3))
+
+funb = lambda *x: x[-1]
+print(funb(12))
+
+L = [lambda x: x+2, lambda x: x*2, lambda x: x**2]
+print("L=", L[0](1), L[1](2), L[2](3))
+
+
 
 # 列表转成字典
 a = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
@@ -26,11 +47,8 @@ print(a['sape'])
 print(a['jack'])
 
 
-def f(x): return x % 2 != 0 and x % 3 != 0
-a = filter(f, range(2, 100))
-print(list(a))
+dict1 = {'Name': 'Zara', 'Age': 7}
+dict2 = {'Sex': 'female' }
 
-
-def cube(x): return x*x*x;
-a = map(cube, range(1,11))
-print(list(a))
+dict1.update(dict2)
+print("Value : %s" %  dict1)
