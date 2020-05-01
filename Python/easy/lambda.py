9383cc0efc9defb5561383284c2f2517a6c47cdd
zhangyun007 <zhaoyun004@qq.com>
2020-05-01 12:17:31 +0800

    mod

--- Python/easy/lambda.py
+++ Python/easy/lambda.py
@@ -3,7 +3,6 @@ x, y = y, x
 print(x)
 print(y)
 
-
 def fibana(n):
 	if n == 1 or n == 2:
 		return 1
@@ -17,12 +16,5 @@ print(fibana(4))
 print(fibana(5))
 print(fibana(6))
 
-
-fiba = lambda n : 1 if n ==1 or n == 2  else fiba(n-1) + fiba(n-2)
-
-print(fiba(1))
-print(fiba(2))
-print(fiba(3))
-print(fiba(4))
-print(fiba(5))
-print(fiba(6))
+L = [lambda x: x+2, lambda x: x*2, lambda x: x**2]
+print("L=", L[0](1), L[1](2), L[2](3))
