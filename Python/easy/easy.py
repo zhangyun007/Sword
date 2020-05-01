a6f925640607a5d8c194c787d525b49c12f806c1
zhangyun007 <zhaoyun004@qq.com>
2020-05-01 12:57:40 +0800

    mod

--- Python/easy/easy.py
+++ Python/easy/easy.py
@@ -11,12 +11,13 @@ print(bigmuls)
 
 print([(x,y) for x in (1,2,3,4) for y in (10,15,3,22) if x*y > 25]) 
 
+L = [lambda x: x+2, lambda x: x*2, lambda x: x**2]
+print "L=", L[0](1), L[1](2), L[2](3)
 
 a = [12, 23, 34, 345, 345, 234 , 234, 435, 345];
 print(a[2:4])
 del(a[2:4]);
 a.remove(234)
-
 
 # 列表转成字典
 a = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
