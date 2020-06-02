2d1c149d9a87228634c7ed5a9acf4cfe093bf1a6
zhangyun007 <zhaoyun004@qq.com>
2020-06-02 19:55:29 +0800

    mod

---
+++ Cpp/EasyGUI/test.s
@@ -0,0 +1,12 @@
+(window ((name "first") (Top "100") (Left "100") (Width "100") (Height "100"))
+  (tabview
+    (div "First Div")
+  )
+  (tabview
+    (div "Second Div")
+  )
+)
+
+(window (name "second")
+  "Hello Second!"
+)
