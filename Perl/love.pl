08c0e7e521971e35d9c17b07634a4e0c55e99925
zhaoyun007 <zhaoyun004@qq.com>
2020-10-10 09:25:26 +0800

    mpod

---
+++ Perl/love.pl
@@ -0,0 +1,15 @@
+listen me, please;
+my $dear;
+
+please:
+'touch my heart'. read the, $message, in;
+
+sort my @feelings, and pop my @feelings;
+
+please:
+open your, heart;
+join us, togeter.
+
+let us tell world,
+	that we are in love until time;
+	
