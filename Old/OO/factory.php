c26d4124106d1a54b36af41b2c46dfbf8633270f
zhaoyun007 <zhaoyun004@qq.com>
2021-04-19 20:55:22 +0800

    mod

--- Old/OO/factory.php
+++ Old/OO/factory.php
@@ -4,32 +4,32 @@
  *简单工厂模式（静态工厂方法模式）
  */
 /**
- * Interface people 人类
+ * Interface clothes 衣服
  */
-interface  people
+interface  Clothes
 {
     public function  say();
 }
 /**
- * Class man 继承people的男人类
+ * Class Coat 继承people的男人类
  */
-class man implements people
+class Coat implements Clothes
 {
     // 具体实现people的say方法
     public function say()
     {
-        echo '我是男人<br>';
+        echo '我是Coat<br>';
     }
 }
 /**
  * Class women 继承people的女人类
  */
-class women implements people
+class Tshirt implements Clothes
 {
     // 具体实现people的say方法
     public function say()
     {
-        echo '我是女人<br>';
+        echo '我是Tshirt<br>';
     }
 }
 /**
@@ -38,22 +38,22 @@ class women implements people
 class SimpleFactoty
 {
     // 简单工厂里的静态方法-用于创建男人对象
-    static function createMan()
+    static function createCoat()
     {
-        return new man();
+        return new Coat();
     }
     // 简单工厂里的静态方法-用于创建女人对象
-    static function createWomen()
+    static function createTshirt()
     {
-        return new women();
+        return new Tshirt();
     }
 }
 /**
  * 直接new一个对象就好，何必多写几行代码多此一举？
  */
-$man = SimpleFactoty::createMan();
-$man->say();
-$woman = SimpleFactoty::createWomen();
-$woman->say();
+$coat = SimpleFactoty::createCoat();
+$coat->say();
+$shirt = SimpleFactoty::createTshirt();
+$shirt->say();
 
 ?>
